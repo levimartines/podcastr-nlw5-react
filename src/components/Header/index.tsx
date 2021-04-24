e8bc3b7d14533490c5491a9a0a90509e5373ec0d
Levi Martines <levi.mferreira@gmail.com>
2021-04-24 16:22:35 -0300

    NLW - Trilha ReactJS - Aula 3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './styles.module.scss';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 
 export function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {locale: ptBR});
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr logo"/>
+      <Link href={`/`}>
+        <img src="/logo.svg" alt="Podcastr logo"/>
+      </Link>
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
     </header>
